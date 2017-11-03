795c08ed3f2ffd77906da930e1b32f1a85e8d3b3
Henkel <George.Henkel@capitalone.com>
2017-11-03 13:45:52 -0400

    Changed inititialization of ATM objects

--- src/ATM.py
+++ src/ATM.py
@@ -3,55 +3,22 @@ import requests
 class ATM(object):
 
     def __init__(self, jsonData):
-        if '_id' in jsonData:
-            self.atmId = jsonData['_id']
-        else:
-            self.atmId = None
-        if 'name' in jsonData:
-            self.name = jsonData['name']
-        else:
-            self.name = None
-        if 'language_list' in jsonData:
-            self.langList = jsonData['language_list']
-        else:
-            self.langList = None
-        if 'address' in jsonData:
-            self.address = jsonData['address']
-        else:
-            self.address = None
-        if 'amount_left' in jsonData:
-            self.amountLeft = jsonData['amount_left']
-        else:
-            self.amountLeft = None
-        if 'accessibility' in jsonData:
-            self.accessibility = jsonData['accessibility']
-        else:
-            self.accessibility = None
-        if 'hours' in jsonData:
-            self.hours = jsonData['hours']
-        else:
-            self.hours = None
-        if 'geocode' in jsonData:
-            geocode = {}
-            if 'lat' in jsonData['geocode']:
-                geocode['lat'] = jsonData['geocode']['lat']
-            else:
-                geocode['lat'] = None
-            if 'lng' in jsonData['geocode']:
-                geocode['lng'] = jsonData['geocode']['lng']
-            else:
-                geocode['lng'] = None
-            self.geocode = geocode
-        else:
-            self.geocode = None
+        self.atmId = jsonData.get('_id')
+        self.name = jsonData.get('name')
+        self.language_list = jsonData.get('language_list')
+        self.address = jsonData.get('address')
+        self.amount_left = jsonData.get('amount_left')
+        self.accessibility = jsonData.get('accessibility')
+        self.hours = jsonData.get('hours')
+        self.geocode = jsonData.get('geocode')
 
     def toDict(self):
         returnDict = {}
         returnDict['_id'] = self.atmId
         returnDict['name'] = self.name
-        returnDict['language_list'] = self.langList
+        returnDict['language_list'] = self.language_list
         returnDict['address'] = self.address
-        returnDict['amount_left'] = self.amountLeft
+        returnDict['amount_left'] = self.amount_left
         returnDict['accessibility'] = self.accessibility
         returnDict['hours'] = self.hours
         returnDict['geocode'] = self.geocode
