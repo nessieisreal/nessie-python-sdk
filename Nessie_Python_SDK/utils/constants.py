0e2ae489ffc35d5dd49d21974139d9e0e95ad966
SkyrmionJ <Jason.Sze1@gmail.com>
2017-11-23 00:10:48 -0500

    Fixing PEP8 for constants.py

--- Nessie_Python_SDK/utils/constants.py
+++ Nessie_Python_SDK/utils/constants.py
@@ -1,10 +1,10 @@
-#Urls
+# Urls
 baseUrl = "http://api.reimaginebanking.com"
 customersUrl = baseUrl + "/customers"
 accountsUrl = baseUrl + "/accounts"
 customersIdUrl = customersUrl + "/%s"
 accountsCustomerIdUrl = accountsUrl + "/%s/customer"
 
-#Exception Messages
+# Exception Messages
 missingFields = 1
 invalidFields = 2
