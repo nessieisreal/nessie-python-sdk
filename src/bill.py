95dc37f226f5bac9c928d330b0b16d39b774ee23
Wesley Lin <wesleylin625@gmail.com>
2017-12-04 22:18:50 -0500

    adding tests and bill scaffolding

---
+++ src/bill.py
@@ -0,0 +1,14 @@
+import json
+import requests
+
+class Bill():
+    def __init__(self):
+    
+    def get_account_bills(self, account_id):
+        pass
+
+    def get_bill(self, bill_id):
+        pass
+
+    def get_customer_bills(self, customer_id):
+        pass
