8c5117f6dabe6555748d43b38ebc6694eb46bd82
Wesley Lin <wesleylin625@gmail.com>
2017-12-08 07:23:26 -0500

    adding bill tests

--- src/bill.py
+++ src/bill.py
@@ -10,29 +10,36 @@ class Bill():
         pass
     
     def get_account_bills(self, account_id):
+        url = f'{self.base_url}/accounts/{account_id}/bills/?key={self.key}'
         pass
 
+
+    # Get the bill using the bill_id
+    # will return
+    
     def get_bill(self, bill_id):
         url = f'{self.base_url}/bills/{bill_id}?key={self.key}' 
         # "/customers/56c66be5a73e492741507273/bills?key="
         response = requests.get(url)
-        if (response.status_code != "200"):
-            error_handle(response)
-        return response.json()
+        # if (response.status_code != 200):
+        #    error_handle(response)
+        result = response.json()
+        result['bill_id']=result['_id']
+        del result['_id']
+        return result
 
     def get_customer_bills(self, customer_id):
         url = f'{self.base_url}/customers/{customer_id}/bills?key={self.key}'
         response = requests.get(url)
-        # if response.status_code != "200":
-        #     raise Exception(response.json()) 
+        #error_handle(response)
         return response.json()
 
 
-def error_handle(error_json):
-    if error_json
+def error_handle(error_response):
+    raise Exception(error_response.json())
 
-# b = Bill('24bb950537c1164a2fbb1bf2a37c3267')
-# bi = b.get_bill("5a261c3883a71c405074fcbd")
-# print(bi)
+b = Bill('24bb950537c1164a2fbb1bf2a37c3267')
+bi = b.get_bill("5a261c3883a71c405074fcbd")
+print(bi)
 # r = b.get_customer_bills("5a2614e483a71c405074fcba")
 # print(r)
--- src/test_bill.py
+++ src/test_bill.py
@@ -4,14 +4,22 @@ import unittest
 class TestBill(unittest.TestCase):
 
     def test_get_bill_succeed(self):
+        print("test succeed")
         bill_factory = bill.Bill("24bb950537c1164a2fbb1bf2a37c3267")
         result = bill_factory.get_bill("5a261c3883a71c405074fcbd")
-        test_result = {'_id': '5a261c3883a71c405074fcbd', 'status': 'pending', 'payee': 'string', 'nickname': 'string', 'payment_date': '2017-12-05', 'recurring_date': 1, 'payment_amount': 23, 'creation_date': '2017-12-05', 'account_id': '5a261a0483a71c405074fcbc'}
-        self.assertEqual(result, test_result)
+        expected_result = {'_id': '5a261c3883a71c405074fcbd', 'status': 'pending', 'payee': 'string', 'nickname': 'string', 'payment_date': '2017-12-05', 'recurring_date': 1, 'payment_amount': 23, 'creation_date': '2017-12-05', 'account_id': '5a261a0483a71c405074fcbc'}
+        self.assertEqual(result, expected_result)
 
     # try fetching a bill that doesn't exist
-    def test_get_bill_fail(self):
+    def test_get_nonreal_bill_fail(self):
         bill_factory = bill.Bill("24bb950537c11642fbb1bf2a37c3267")
+        # err = {}
+        # try:
+        #     result = bill_factory.get_bill("fake")
+        # except Exception as err:
+
+        #     pass
         result = bill_factory.get_bill("fake")
-        print(result)
+        expected_result = {'code':401, 'message':'unauthorized'}
+        self.assertEqual(result, expected_result)
         
