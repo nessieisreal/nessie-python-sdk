21afdf06a5ec61aaeb834ea0af0402b71889627e
Wesley LIn <wesleylin625@gmail.com>
2018-01-23 15:14:54 +0000

    fix url

--- nessie/dataRequests.py
+++ nessie/dataRequests.py
@@ -6,8 +6,12 @@ class DataRequest():
     def __init__(self,api_key):
         self.key = api_key
 
+    # Can Delete:
+    # Accounts, Bills, Customers, Deposits
+    # Loans, Purchases, Transfers, Withdrawals
+
     def delete_data(self, dataType:str):
-        url=f'{baseUrl}/data/?type={dataType}&?key={self.key}'
+        url=f'{baseUrl}/data?type={dataType}&?key={self.key}'
         response = requests.delete(url)
         # if (response.status_code != 200):
         #    error_handle(response)
