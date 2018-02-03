098bd23e8050ed33860da106a5757428b52afa9f
Wesley LIn <wesleylin625@gmail.com>
2018-02-03 00:49:40 +0000

    removed old tests

--- tests/test_bill_requests.py
+++ tests/test_bill_requests.py
@@ -35,16 +35,7 @@ class TestBillRequests(unittest.TestCase):
 
     def tearDown(self):
         data_deletor = DataRequests(wkey)
-        # response = data_deletor.delete_data('Bills')
 
-    # def test_get_bill_succeed(self):
-    #     print("test_get_bill_succeed")
-    #     bill_factory = billRequests.BillRequest(wkey)
-    #     result = bill_factory.get_account_bills(account_id)
-    #     print(result)
-    #     # result = bill_factory.get_bill("5a261c3883a71c405074fcbd")
-    #     # expected_result = {'bill_id': '5a261c3883a71c405074fcbd', 'status': 'pending', 'payee': 'string', 'nickname': 'string', 'payment_date': '2017-12-05', 'recurring_date': 1, 'payment_amount': 23, 'creation_date': '2017-12-05', 'account_id': '5a261a0483a71c405074fcbc'}
-    #     self.assertEqual(result[0]['payment_date'], '2018-01-10')
 
     # try fetching a bill that doesn't exist
     def test_get_nonreal_bill_fail(self):
